3fc234400d26375b383c67f56d01816317f00960
Maxwell You <maxwell.you@mlb.com>
2024-01-22 12:25:35 -0700

    add working <Svg>

--- App.tsx
+++ App.tsx
@@ -1,17 +1,36 @@
 import "react-native-svg";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { SvgUri } from "react-native-svg";
+import { Svg, Circle, Rect, SvgUri } from "react-native-svg";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <SvgUri
+      {/* <SvgUri
         uri="https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/tiger.svg"
         width={36}
         height={36}
-      />
+      /> */}
+      <Svg height="50%" width="50%" viewBox="0 0 100 100">
+        <Circle
+          cx="50"
+          cy="50"
+          r="45"
+          stroke="blue"
+          strokeWidth="2.5"
+          fill="green"
+        />
+        <Rect
+          x="15"
+          y="15"
+          width="70"
+          height="70"
+          stroke="red"
+          strokeWidth="2"
+          fill="yellow"
+        />
+      </Svg>
       <StatusBar style="auto" />
     </View>
   );
